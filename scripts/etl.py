1f6657d2ddd29fb7d0d8c4816dfe88b069a08bc6
Anindya Mahanty <46273941+Anindya711@users.noreply.github.com>
2021-06-28 10:53:03 +0530

    The initial commit

---
+++ scripts/etl.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+
+def extract(spark):
+    try:
+        pass
+
+    except as Exception e:
+        print(e)
+
+def main():
+    print("hello Danone")
+
+if __name__ == "__main__":
+    main()
